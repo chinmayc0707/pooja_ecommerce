9bbb6743f97399e5e8335378539331214503117d
Chinmay C Bhat <chinmayc200@gmail.com>
2026-06-01 12:26:01 +0530

    Added init code for db

--- app.py
+++ app.py
@@ -53,6 +53,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
 # --- Models ---
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
